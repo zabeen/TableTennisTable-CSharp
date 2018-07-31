3a900e612d264da44e07630038b85aad614758ce
Joe Edwards <Joe.Edwards@softwire.com>
2018-07-31 14:01:58 +0100

    Sample App and Integration Tests

---
+++ TableTennisTable-Tests/AppTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Linq;
+using TableTennisTable_CSharp;
+
+namespace TableTennisTable_Tests
+{
+    [TestClass]
+    public class AppTests
+    {
+        [TestMethod]
+        public void TestPrintsCurrentState()
+        {
+            var league = new League();
+            var renderer = new Mock<ILeagueRenderer>();
+            renderer.Setup(r => r.Render(league)).Returns("Rendered League");
+
+            var app = new App(league, renderer.Object, null);
+
+            Assert.AreEqual("Rendered League", app.SendCommand("print"));
+        }
+    }
+}
---
+++ TableTennisTable-Tests/IntegrationTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using TableTennisTable_CSharp;
+
+namespace TableTennisTable_Tests
+{
+    [TestClass]
+    public class IntegrationTests
+    {
+        [TestMethod]
+        public void TestPrintsEmptyGame()
+        {
+            var app = CreateApp();
+
+            Assert.AreEqual("No players yet", app.SendCommand("print"));
+        }
+
+        private App CreateApp()
+        {
+            return new App(new League(), new LeagueRenderer(), new FileService());
+        }
+    }
+}
