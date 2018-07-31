4cad4f8c242415fa24525b462732d2618ca3e06d
Joe Edwards <Joe.Edwards@softwire.com>
2018-07-31 10:48:33 +0100

    App Tests

--- TableTennisTable-Tests/AppTests.cs
+++ TableTennisTable-Tests/AppTests.cs
@@ -20,5 +20,91 @@ namespace TableTennisTable_Tests
 
             Assert.AreEqual("Rendered League", app.SendCommand("print"));
         }
+
+        [TestMethod]
+        public void TestAddsPlayer()
+        {
+            var league = new League();
+            var app = new App(league, null, null);
+
+            app.SendCommand("add player Anna");
+
+            var rows = league.GetRows();
+            Assert.AreEqual(1, rows.Count);
+            var firstRowPlayers = rows.First().GetPlayers();
+            Assert.AreEqual(1, firstRowPlayers.Count);
+            CollectionAssert.Contains(firstRowPlayers, "Anna");
+        }
+
+        [TestMethod]
+        public void TestRecordsWins()
+        {
+            var league = new League();
+            league.AddPlayer("Anna");
+            league.AddPlayer("Bob");
+            var app = new App(league, null, null);
+
+            app.SendCommand("record win Bob Anna");
+
+            var rows = league.GetRows();
+            CollectionAssert.Contains(rows[0].GetPlayers(), "Bob");
+            CollectionAssert.Contains(rows[1].GetPlayers(), "Anna");
+        }
+
+        [TestMethod]
+        public void TestReportsInvalidWins()
+        {
+            var league = new League();
+            league.AddPlayer("Anna");
+            var app = new App(league, null, null);
+
+            Assert.AreEqual("Player Zabine is not in the game", app.SendCommand("record win Anna Zabine"));
+        }
+
+        [TestMethod]
+        public void TestShowsWinner()
+        {
+            var league = new League();
+            league.AddPlayer("Anna");
+            var app = new App(league, null, null);
+
+            Assert.AreEqual("Anna", app.SendCommand("winner"));
+        }
+
+        [TestMethod]
+        public void TestSavesCurrentState()
+        {
+            var league = new League();
+            var fileService = new Mock<IFileService>();
+            var app = new App(league, null, fileService.Object);
+
+            app.SendCommand("save test-tournament");
+
+            fileService.Verify(f => f.Save("test-tournament", league));
+        }
+
+        [TestMethod]
+        public void TestLoadsFromFile()
+        {
+            var league = new League();
+            var leagueToLoad = new League();
+            leagueToLoad.AddPlayer("Anna");
+            var fileService = new Mock<IFileService>();
+            fileService.Setup(f => f.Load("test-tournament")).Returns(leagueToLoad);
+            var app = new App(league, null, fileService.Object);
+
+            app.SendCommand("load test-tournament");
+
+            Assert.AreEqual("Anna", app.SendCommand("winner"));
+        }
+
+        [TestMethod]
+        public void TestInvalidCommands()
+        {
+            var league = new League();
+            var app = new App(league, null, null);
+
+            Assert.AreEqual("Unknown command: invalid", app.SendCommand("invalid"));
+        }
     }
 }
