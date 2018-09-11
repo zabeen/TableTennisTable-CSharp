5fbf87e36dbf90cb4ed6a5f93b591a4ef650298c
Joe Edwards <Joe.Edwards@softwire.com>
2018-09-11 11:31:29 +0100

    Add simple SpecFlow test

---
+++ TableTennisTable-Tests/Game.feature
@@ -0,0 +1,6 @@
+﻿Feature: Game
+
+    Scenario: Empty League
+	    Given the league has no players
+	    When I print the league
+	    Then I should see "No players yet"
---
+++ TableTennisTable-Tests/GameSteps.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TableTennisTable_CSharp;
+using TechTalk.SpecFlow;
+
+namespace TableTennisTable_Tests
+{
+    [Binding]
+    public class GameSteps
+    {
+        // Current app instance
+        private App app;
+
+        // Last response
+        private String response;
+
+        [BeforeScenario]
+        public void CreateApp()
+        {
+            var league = new League();
+            var leagueRenderer = new LeagueRenderer();
+            var fileService = new FileService();
+            app = new App(league, leagueRenderer, fileService);
+        }
+
+        [Given("the league has no players")]
+        public void GivenTheLeagueHasNoPlayers()
+        {
+            // Nothing to do - the default league starts with no players
+        }
+        
+        [When("I print the league")]
+        public void WhenIPrintTheLeague()
+        {
+            response = app.SendCommand("print");
+        }
+
+        [Then("I should see \"(.*)\"")]
+        public void ThenIShouldSeeThatThereAreNoPlayers(string expected)
+        {
+            Assert.AreEqual(expected, response);
+        }
+    }
+}
